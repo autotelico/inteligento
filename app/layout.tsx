3db92a240d8e487e2607b8c29ec4dc9b5023826a
Henrique Heron <henriqueheron1@gmail.com>
2024-04-20 17:11:48 -0300

    change metadata from static to dynamic in main layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
+import Favicon from '@/public/favicon.ico'
 
 import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Inteligento",
-  description: "Flashcards para estudos. By: Henrique Heron",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  
+  return {
+    title: "Inteligento",
+    description: "Flashcards para estudos. By: Henrique Heron",
+    openGraph: {
+      images: [Favicon.src],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
