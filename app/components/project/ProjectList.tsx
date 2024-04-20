9f7aba2b05ba3899c648897c72f9237a88699d35
Henrique Heron <henriqueheron1@gmail.com>
2024-04-19 23:39:15 -0300

    add blur to the background when modal pops up

--- app/components/project/ProjectList.tsx
+++ app/components/project/ProjectList.tsx
@@ -24,12 +24,18 @@ export default function ProjectList({
 }): JSX.Element {
   const [showModal, setShowModal] = useState<boolean>(false);
 
+  const toggleBlur = (): void => {
+    document.querySelector('#blur')?.classList.toggle('hidden')
+  }
+  
   const toggleShowModal = (): void => {
     setShowModal(!showModal);
+    toggleBlur();
   };
 
   return (
     <div id="project-container">
+      <div id="blur" className="hidden"></div>
       {/* placeholder */}
       <h2>Project List</h2>
       {showModal && (
